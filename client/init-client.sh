89d6b126d76af8acdfb166c761279461174f1161
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-28 23:31:02 +0200

    ...

--- client/init-client.sh
+++ client/init-client.sh
@@ -3,4 +3,4 @@ mkdir $HOME/get-from-host $HOME/push-to-host
 chmod +x start-client.sh
 cp start-client.sh $PREFIX/bin/
 echo "" >> $HOME/.bashrc
-echo start-client.sh >> $HOME/.bashrc
+echo "start-client.sh 2> tmp.txt &" >> $HOME/.bashrc
