179f1e0f4529bac12e64a772524b6fd21ca21b35
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-05-28 23:34:27 +0200

    ...

--- client/init-client.sh
+++ client/init-client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# WARNING! this script ERASES AND OVERWRITES .bashrc !!!
 mkdir $HOME/get-from-host $HOME/push-to-host
 chmod +x start-client.sh
 cp start-client.sh $PREFIX/bin/
-echo "" >> $HOME/.bashrc
-echo "start-client.sh 2> tmp.txt &" >> $HOME/.bashrc
+echo "" > $HOME/.bashrc
+echo "start-client.sh > out.log 2> err.log" >> $HOME/.bashrc
